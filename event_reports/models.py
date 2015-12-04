260a32ecc8761612b92d264d66fd3fbea6ab5eec
Brian Aarts <b.aarts@lukkien.com>
2015-12-04 09:44:52 +0100

    Create custom streamfield for event reports, so that the editor has more customization

--- event_reports/models.py
+++ event_reports/models.py
@@ -3,14 +3,18 @@ from django.db import models
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import StreamField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel, StreamFieldPanel
-from event_reports.blocks import EventReportBlock
+from wagtail.wagtailcore import blocks
+from wagtail.wagtailimages.blocks import ImageChooserBlock
 
 
 class EventReportPage(Page):
     author = models.CharField(max_length=255)
+    event_name = models.CharField(max_length=255)
     date = models.DateField("Post date")
     event_report = StreamField([
-        ('report', EventReportBlock()),
+        ('event_text_block', blocks.RichTextBlock()),
+        ('event_sub_header', blocks.CharBlock()),
+        ('event_picture', ImageChooserBlock())
     ])
 
 EventReportPage.content_panels = [
